3ab0aa0be27a074c4e2a7146f17ed0611e680544
Yuvraj-Tamatta <yuvraj@corbitaltechnologies.com>
2024-12-13 19:05:31 +0530

    Feat: login, register, footer

--- theme/routes/web.php
+++ theme/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/register', function(){
+    return view('Registration');
+})->name('register');
+
+Route::fallback(function () {
+    return view('Erro');
+});
